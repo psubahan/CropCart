25c05084683f971f736b595bc3d8f9b8077adb30
subahan <subahanpulimaddi@gmail.com>
2025-01-21 17:42:00 +0530

    product Details DAO Impli

---
+++ src/main/java/com/cropcart/DAO/ProductDAOImp.java
@@ -0,0 +1,84 @@
+package com.cropcart.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cropcart.db.dbConnection;
+import com.cropcart.dto.ProductCategory;
+import com.cropcart.dto.ProductDetails;
+
+public class ProductDAOImp implements ProductDAO 
+{
+	private Connection con=null;
+	
+	public ProductDAOImp(Connection con) {
+		this.con = dbConnection.getConnection();
+	}
+
+	@Override
+	public ProductDetails getProduct(int product_id) {
+	    PreparedStatement ps = null;
+	    ResultSet rs = null;
+	    String query = "SELECT * FROM product_details WHERE product_id=?";
+	    ProductDetails pd = null;
+
+	    try {
+	        ps = con.prepareStatement(query);
+	        ps.setInt(1, product_id);
+	        rs = ps.executeQuery();
+
+	        if (rs.next()) {
+	            pd = new ProductDetails();
+	            pd.setProduct_id(rs.getInt("product_id")); 
+	            pd.setCategoty(rs.getString("category"));
+	            pd.setTitle(rs.getString("title"));
+	            pd.setImage(rs.getString("image"));
+	            pd.setQuantity(rs.getInt("quantity"));
+	            pd.setDescription(rs.getString("description"));
+	            pd.setQuantity_type(rs.getString("quantity_type"));
+	            pd.setPrice(rs.getLong("price"));
+	            pd.setStatus(rs.getString("status"));
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } 
+	    return pd; 
+	}
+
+
+	@Override
+	public List<ProductDetails> getAllproducts() {
+		ArrayList<ProductDetails>pdl=new ArrayList<ProductDetails>();
+		 PreparedStatement ps = null;
+		    ResultSet rs = null;
+		    String query = "SELECT * FROM product_details";
+		    ProductDetails pd = null;
+
+		    try {
+		        ps = con.prepareStatement(query);
+		        rs = ps.executeQuery();
+
+		        if (rs.next()) {
+		            pd = new ProductDetails();
+		            pd.setProduct_id(rs.getInt("product_id")); 
+		            pd.setCategoty(rs.getString("categoty"));
+		            pd.setTitle(rs.getString("title"));
+		            pd.setImage(rs.getString("image"));
+		            pd.setQuantity(rs.getInt("quantity"));
+		            pd.setDescription(rs.getString("description"));
+		            pd.setQuantity_type(rs.getString("quantity_type"));
+		            pd.setPrice(rs.getLong("price"));
+		            pd.setStatus(rs.getString("status"));
+		            pdl.add(pd);
+		        }
+		    } catch (SQLException e) {
+		        e.printStackTrace();
+		    } 
+		    return pdl; 
+	}
+	
+}
